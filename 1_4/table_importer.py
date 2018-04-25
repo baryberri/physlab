9f99e69e93892eea0c609b0e8bff5b8202608d19
baryberri <baryberri@icloud.com>
2018-04-26 02:09:03 +0900

    Update header-footer logic for gyroscope dataset

--- 1_4/table_importer.py
+++ 1_4/table_importer.py
@@ -7,7 +7,7 @@ class TableImporter:
         file_urls = [os.path.join(directory, filename) for filename in file_names]
         self.files = file_urls
 
-    def get_table(self, delimiter="\t", header=True):
+    def get_table(self, delimiter="\t", header=0, footer=0):
         if len(self.files) == 0:
             return None
 
@@ -16,10 +16,13 @@ class TableImporter:
         with open(file_url, "r", encoding='cp949') as file:
             table = file.readlines()
 
-            if header:
+            for _ in range(header):
                 del table[0]
 
+            for _ in range(footer):
+                del table[-1]
+
             split_table = [table_row.strip().split(delimiter) for table_row in table]
-            float_table = [[float(data) for data in table_row] for table_row in split_table]
+            float_table = [[float(data.strip()) for data in table_row] for table_row in split_table]
 
         return float_table
