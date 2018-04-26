b0d345f95f7b255fb051aa6dd0df447358a32728
baryberri <baryberri@icloud.com>
2018-04-26 09:59:50 +0900

    Implement physlab 1-4 error checker

--- 1_4/process.py
+++ 1_4/process.py
@@ -4,36 +4,25 @@ from compute import *
 
 
 def main():
-    r = 0.038 / 2
-    d = 0.024
-    mark = 30
-
     tables = TableImporter()
     writer = CSVWriter()
+    table = tables.get_table(delimiter=",", header=5, footer=7)
 
-    table = tables.get_table()
-
-    writer.string_write(["theory", "exp", "error"])
+    error_rate_result = []
+    step = 4
 
     while table is not None:
-        times = get_1d_data(table, 0)
-        ys = get_1d_data(table, 2)
-        h = get_h(mark)
-        valid_ys = filter_descending_movement(ys)
-        valid_ys_in_meter = change_to_meter(valid_ys)
+        consistent_table = extract_consistent_row(table, 3)
+        w = rpm_to_rad_per_sec(consistent_table[0][3])
 
-        start_point = (times[1], valid_ys_in_meter[1])
-        end_point = (times[len(valid_ys) - 2], valid_ys_in_meter[len(valid_ys_in_meter) - 2])
+        theory_rotation = compute_theory_rotation(step_to_length(step), w)
+        experiment_rotation = get_average_rotation(consistent_table)
 
-        exp_time = end_point[0] - start_point[0]
-        theory_time = theoretical_time(start_point, end_point, h, r, d)
-        error = (exp_time - theory_time) / theory_time * 100
+        error_rate_result.append((experiment_rotation - theory_rotation) / theory_rotation * 100)
 
-        writer.write([theory_time, exp_time, error])
+        table = tables.get_table(delimiter=",", header=5, footer=7)
 
-        table = tables.get_table()
-
-
+    writer.write(error_rate_result)
 
 if __name__ == '__main__':
     main()
