016685c1a3260bd41b0184f2c77d8fd4596a1ba7
baryberri <baryberri@icloud.com>
2018-05-03 06:18:34 +0900

    Implement to compute v1, v2 at around turning point

--- 1_5/b/compute.py
+++ 1_5/b/compute.py
@@ -1,39 +1,21 @@
 import math
 
 
-def get_1d_data(table, index):
+def get_column(table, index):
     result = []
     for row in table:
         result.append(row[index])
     return result
 
 
-def filter_descending_movement(data):
-    result = []
-    for i in range(len(data)):
-        if data[i] > data[i + 1]:
-            result.append(data[i])
-        else:
-            break
-    return result
-
-
-def change_to_meter(data):
-    result = []
-    for h in data:
-        h_in_meter = 0.01 * (h + 9.577) + 0.68242641
-        result.append(h_in_meter)
-    return result
-
-
-def theoretical_time(start_point, end_point, h, r, d):
-    reff_squared = (r ** 2) - ((d / 2) ** 2)
-    k = (r ** 2) / reff_squared
-    g = 9.80665
-    start_h = start_point[1]
-    end_h = end_point[1]
-    return math.sqrt((2 + 0.8 * k) / g) * (2 / math.sqrt(2)) * (((h - end_h) ** 0.5) - ((h - start_h) ** 0.5))
-
-
-def get_h(mark):
-    return 0.64 + 0.01 * (10 - mark) * (math.sqrt(2) / 2)
+def get_speed(xs, ys, time, index, step):
+    next_index = index + step
+    if next_index < 0:
+        next_index = 0
+    if next_index >= len(time):
+        next_index = len(time) - 1
+    dx = (xs[next_index] - xs[index]) / 100
+    dy = (ys[next_index] - ys[index]) / 100
+    dt = abs(time[next_index] - time[index])
+    distance = math.sqrt(dx * dx + dy * dy)
+    return distance / dt
