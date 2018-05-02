886d66db04075e292a5da126f3d138575534b12e
baryberri <baryberri@icloud.com>
2018-05-03 06:09:39 +0900

    Update logic to compute unknown mass

--- 1_5/a/compute.py
+++ 1_5/a/compute.py
@@ -8,16 +8,46 @@ def get_column(table, index):
     return result
 
 
-def get_abs_dx(xs, time):
-    return abs(xs[time] - xs[time + 1])
-
-
-def get_degree(xs, ys, time):
+def get_distance(xs, ys, time):
     dx = xs[time + 1] - xs[time]
-    if dx == 0:
-        return None
-
     dy = ys[time + 1] - ys[time]
+    return math.sqrt(dx * dx + dy * dy)
 
-    degree_in_rad = math.atan(dy / dx)
-    return math.degrees(degree_in_rad)
+
+def get_vector(xs, ys, time):
+    dx = xs[time + 4] - xs[time + 1]
+    dy = ys[time + 4] - ys[time + 1]
+    return dx, dy
+
+
+def get_speed(xs, ys, time, index, step):
+    next_index = index + step
+    if next_index < 0:
+        next_index = 0
+    elif next_index >= len(time):
+        next_index = len(time) - 1
+    dx = (xs[next_index] - xs[index]) / 100
+    dy = (ys[next_index] - ys[index]) / 100
+    dt = abs(time[next_index] - time[index])
+    distance = math.sqrt(dx * dx + dy * dy)
+    return distance / dt
+
+
+def get_vector_size(v):
+    return math.sqrt(v[0] * v[0] + v[1] * v[1])
+
+
+def get_degree(v1, v2):
+    v1_size = get_vector_size(v1)
+    v2_size = get_vector_size(v2)
+    inner_product = v1[0] * v2[0] + v1[1] * v2[1]
+    return math.degrees(math.acos(inner_product / (v1_size * v2_size)))
+
+
+def get_mass(v, v1, v2, b):
+    m = 0.045
+    a = v2 * v2
+    b = m * v1 * v2 * math.cos(math.radians(b))
+    c = m * m * (v1 * v1 - v * v)
+    result = (-1 * b + math.sqrt(b * b - a * c)) / a
+    return (result - 0.045) * 1000
--- 1_5/a/process.py
+++ 1_5/a/process.py
@@ -5,28 +5,31 @@ from compute import *
 
 def main():
     tables = TableImporter()
-    writer = CSVWriter()
 
     table = tables.get_table()
 
     while table is not None:
         time = get_column(table, 0)
+        static_xs = get_column(table, 1)
+        static_ys = get_column(table, 2)
         moving_xs = get_column(table, 3)
         moving_ys = get_column(table, 4)
 
-        past_degree = get_degree(moving_xs, moving_ys, 0)
-        past_abs_dx = 0
-        for i in range(1, len(table)):
-            deg = get_degree(moving_xs, moving_ys, i)
-            if deg is None:
-                continue
-
-            if abs(past_degree - deg) >= 10 and past_abs_dx > 1:
-                print(i, time[i])
+        start_index = None
+        for i in range(len(table) - 1):
+            if get_distance(static_xs, static_ys, i) > 0.65:
+                start_index = i + 1
                 break
 
-            past_degree = deg
-            past_abs_dx = get_abs_dx(moving_xs, i)
+        moving_vector = get_vector(moving_xs, moving_ys, start_index)
+        static_vector = get_vector(static_xs, static_ys, start_index)
+        observed_beta = get_degree(moving_vector, static_vector)
+
+        past_speed = get_speed(moving_xs, moving_ys, time, start_index - 2, -3)
+        moving_speed = get_speed(moving_xs, moving_ys, time, start_index + 2, 3)
+        static_speed = get_speed(static_xs, static_ys, time, start_index + 2, 3)
+
+        print(get_mass(past_speed, moving_speed, static_speed, observed_beta))
 
         table = tables.get_table()
 
